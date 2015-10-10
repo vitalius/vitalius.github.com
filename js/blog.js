b8d8cb4138a5fe06064e6451f396cc3e11705a52
vitaliy <vitaliy@cloudmotion.com>
2015-10-09 21:56:57 -0700

    re-style

--- js/blog.js
+++ js/blog.js
@@ -1,8 +1,9 @@
+/**
+ * Get URL params
+ */ 
 var getUrlParameter = function getUrlParameter(sParam) {
     var sPageURL = decodeURIComponent(window.location.search.substring(1)),
-        sURLVariables = sPageURL.split('&'),
-        sParameterName,
-        i;
+    sURLVariables = sPageURL.split('&'), sParameterName, i;
 
     for (i = 0; i < sURLVariables.length; i++) {
         sParameterName = sURLVariables[i].split('=');
@@ -14,37 +15,42 @@ var getUrlParameter = function getUrlParameter(sParam) {
 };    
 
 
-// the entire framework right here!
+/**
+ * Render post file
+ */
+function renderPost(title, date, file) {
+    
+    $("#post_title").append(title);
+    
+    if (date)
+        $("#post_date").append("Posted on " + date); 
+    
+    $("#post_body").load("content/" + file, function( response, status, xhr ) {
+        if ( status == "error" ) {
+           var msg = "Sorry but there was an error: ";
+           $( "#post_body" ).html( msg + xhr.status + " " + xhr.statusText );
+        } else {              
+           MathJax.Hub.Config({ tex2jax: { inlineMath: [ ['$','$'], ['\\(','\\)'] ],
+                                           displayMath: [ ['$$','$$'], ['\\[','\\]'] ],
+                                           processEscapes: true } });  
+           MathJax.Hub.Queue(["Typeset",MathJax.Hub]);
+        }                
+    });
+}
+
+
+/**
+ * On load
+ */
 $.getJSON("content/index.json", function(post_list) { 
+    
     var post_file = getUrlParameter("p"); 
-    if (post_file) {
-      // render a post
-      $("#content").load("post.html", function() {
-          $("#post_title").append(post_list[post_file].title);
-          $("#post_date").append(post_list[post_file].date); 
-          $("#post_body").load("content/" + post_file, function( response, status, xhr ) {
-              if ( status == "error" ) {
-                 var msg = "Sorry but there was an error: ";
-                 $( "#post_body" ).html( msg + xhr.status + " " + xhr.statusText );
-              } else {              
-                 // run MathJax and code highlight processors
-                 //$('pre code').each(function(i, block) { hljs.highlightBlock(block); });
-                 MathJax.Hub.Config({ tex2jax: { inlineMath: [ ['$','$'], ['\\(','\\)'] ],
-                                                 displayMath: [ ['$$','$$'], ['\\[','\\]'] ],
-                                                 processEscapes: true } });  
-                 MathJax.Hub.Queue(["Typeset",MathJax.Hub]);
-              }                
-          });
-      });
-    } else {
-      // render post index
-      $("#content").load("list.html", function() {
-          $.each(post_list, function(k,v) { $("#post_list").append(
-                "<li class=\"list-group-item\"><a href=\"?p=" + k + "\">" + v.title +"</a>" +
-                "  <span class=\"pull-right\">" + v.date + "</span>" +
-                "</li>")
-          });
-      });
-    }
+    
+    if (!post_file)
+        post_file = Object.keys(post_list)[0];
+    
+    $("#content").load("post.html", function() {
+        renderPost(post_list[post_file].title, post_list[post_file].date, post_file);
+    });
 });
 

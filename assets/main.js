650de9de562e432bbfeef960618ab8ae97412447
vitaliy <vitaliy@cloudmotion.com>
2015-12-16 21:35:48 -0800

    routes, colors

--- assets/main.js
+++ assets/main.js
@@ -1,24 +1,32 @@
 /**
- * Get URL params
- */ 
-var getUrlParameter = function getUrlParameter(sParam) {
-    var sPageURL = decodeURIComponent(window.location.search.substring(1)),
-    sURLVariables = sPageURL.split('&'), sParameterName, i;
+ * Cool colors
+ */
 
-    for (i = 0; i < sURLVariables.length; i++) {
-        sParameterName = sURLVariables[i].split('=');
+var color = [  
+    [62,35,255],
+    [60,255,60],
+    [255,35,98],
+    [45,175,230],
+    [255,0,255],
+    [255,128,0]
+];
 
-        if (sParameterName[0] === sParam) {
-            return sParameterName[1] === undefined ? true : sParameterName[1];
-        }
+
+/**
+ * Route
+ */
+function routePage() {
+    var page = RegExp('\\?\\w+\\.\\w+', 'i').exec(window.location)
+    if (page) {
+        return page[0].replace('?', '')
     }
-};    
+}
 
 
 /**
  * Render post file
  */
-function renderPost(title, date, file) {
+function renderPage(title, date, file) {
     
     $("#post_title").append(title);
     
@@ -42,31 +50,45 @@ function renderPost(title, date, file) {
 
 
 /**
+ * Add color zaz
+ */
+function mouseActive(o) {   
+    var r = Math.floor(Math.random()*color.length);
+
+    o.on('mouseover', function() { 
+        o.attr("fill", "rgb(" + color[r] + ")" );
+        o.attr("stroke-width", "3" );
+        o.attr("stroke", "rgb(" + color[r] + ")" ); 
+    });
+    
+    o.on('mouseout', function() { 
+        o.attr("fill", "#000" );
+        o.attr("stroke", "0" );  
+    });  
+}
+
+
+
+/**
  * On load
  */
-$.getJSON("content/index.json", function(post_list) { 
+$.getJSON("content/index.json", function(post_list) {    
+    var page_file = routePage();
     
-    var post_file = getUrlParameter("p"); 
-    
-    if (!post_file)
-        post_file = Object.keys(post_list)[0];
+    if (!page_file)
+        page_file = Object.keys(post_list)[0]; // first item off the list
     
     $("#content").load("post.html", function() {
-        renderPost(post_list[post_file].title, post_list[post_file].date, post_file);
+        renderPage(post_list[page_file].title, post_list[page_file].date, page_file);
     });    
     
-    var color = [  
-        [62,35,255],
-        [60,255,60],
-        [255,35,98],
-        [45,175,230],
-        [255,0,255],
-        [255,128,0]
-    ];
     var i = Math.floor(Math.random() * (color.length - 1));    
-    //$("#header_logo").css("color", "rgb(" + color[i] + ")");
     $("#header_bar").css("background", "-webkit-linear-gradient(left, rgb(" + color[i] + "), rgb(" + color[i + 1] + "))");
     $("#header_bar").css("background", "-o-linear-gradient(right, rgb(" + color[i] + "), rgb(" + color[i + 1] + "))");
     $("#header_bar").css("background", "-moz-linear-gradient(right, rgb(" + color[i] + "), rgb(" + color[i + 1] + "))");
     $("#header_bar").css("background", "linear-gradient(to right, rgb(" + color[i] + "), rgb(" + color[i + 1] + "))");
+
+    /* logo colors */
+    for(var i=0; i<9; i++)
+        mouseActive($('#'+i));
 });

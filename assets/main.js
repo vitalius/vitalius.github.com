55245fb2a3fbf2ac0beb9ad884e93ddf8948a243
vitaliy <vitaliy@cloudmotion.com>
2015-12-17 19:59:02 -0800

    some js refactor

--- assets/main.js
+++ assets/main.js
@@ -1,50 +1,25 @@
 /**
- * Cool colors
- */
-
-var color = [  
-    [62,35,255],
-    [60,255,60],
-    [255,35,98],
-    [45,175,230],
-    [255,0,255],
-    [255,128,0]
-];
-
-
-/**
  * Route
  */
 function routePage() {
     var page = RegExp('\\?\\w+\\.\\w+', 'i').exec(window.location)
-    if (page) {
-        return page[0].replace('?', '')
-    }
+    if (page)
+        return page[0].replace('?', '');
 }
 
 
 /**
  * Render post file
  */
-function renderPage(title, date, file) {
-    
+function renderPage(title, date, file) {  
     $("#post_title").append(title);
     
     if (date)
         $("#post_date").append("Posted on " + date); 
     
     $("#post_body").load("content/" + file, function( response, status, xhr ) {
-        if ( status == "error" ) {
-           var msg = "Sorry but there was an error: ";
-           $( "#post_body" ).html( msg + xhr.status + " " + xhr.statusText );
-        } else {              
-            /*
-           MathJax.Hub.Config({ tex2jax: { inlineMath: [ ['$','$'], ['\\(','\\)'] ],
-                                           displayMath: [ ['$$','$$'], ['\\[','\\]'] ],
-                                           processEscapes: true } });  
-           MathJax.Hub.Queue(["Typeset",MathJax.Hub]);
-           */
-        }                
+        if ( status == "error" )
+           $( "#post_body" ).html( "Sorry but there was an error: " + xhr.status + " " + xhr.statusText );           
     });
 }
 
@@ -62,10 +37,25 @@ $.getJSON("content/index.json", function(post_list) {
     $("#content").load("post.html", function() {
         renderPage(post_list[page_file].title, post_list[page_file].date, page_file);
     });    
-    
-    var i = Math.floor(Math.random() * (color.length - 1));    
-    $("#header_bar").css("background", "-webkit-linear-gradient(left, rgb(" + color[i] + "), rgb(" + color[i + 1] + "))");
-    $("#header_bar").css("background", "-o-linear-gradient(right, rgb(" + color[i] + "), rgb(" + color[i + 1] + "))");
-    $("#header_bar").css("background", "-moz-linear-gradient(right, rgb(" + color[i] + "), rgb(" + color[i + 1] + "))");
-    $("#header_bar").css("background", "linear-gradient(to right, rgb(" + color[i] + "), rgb(" + color[i + 1] + "))");
 });
+
+
+
+/**
+ * I like these colors
+ */
+var color = [  
+    [62,35,255],
+    [60,255,60],
+    [255,35,98],
+    [45,175,230],
+    [255,0,255],
+    [255,128,0]
+];
+
+var c = Math.floor(Math.random() * (color.length - 1));
+
+$("#header_bar").css("background", "-webkit-linear-gradient(left, rgb(" + color[c] + "), rgb(" + color[c + 1] + "))");
+$("#header_bar").css("background", "-o-linear-gradient(right, rgb(" + color[c] + "), rgb(" + color[c + 1] + "))");
+$("#header_bar").css("background", "-moz-linear-gradient(right, rgb(" + color[c] + "), rgb(" + color[c + 1] + "))");
+$("#header_bar").css("background", "linear-gradient(to right, rgb(" + color[c] + "), rgb(" + color[c + 1] + "))");

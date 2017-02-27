ca0e5ca52b6e445a193c65d2d88a3895f60a20a5
Vitaliy <vitaliy@cloudmotion.com>
2017-02-26 21:23:43 -0800

    Update main.js

--- assets/main.js
+++ assets/main.js
@@ -3,8 +3,7 @@
  */
 function routePage() {
     var page = RegExp('\\?\\w+\\.\\w+', 'i').exec(window.location)
-    if (page)
-        return page[0].replace('?', '');
+    if (page) return page[0].replace('?', '');
 }
 
 
@@ -14,12 +13,11 @@ function routePage() {
 function renderPage(title, date, file) {  
     $("#post_title").append(title);
     
-    if (date)
-        $("#post_date").append("Posted on " + date); 
+    if (date) $("#post_date").append(date); 
     
     $("#post_body").load("content/" + file, function( response, status, xhr ) {
         if ( status == "error" )
-           $( "#post_body" ).html( "Sorry but there was an error: " + xhr.status + " " + xhr.statusText );           
+           $( "#post_body" ).html( "Error: " + xhr.status + " " + xhr.statusText );           
     });
 }
 
@@ -31,8 +29,7 @@ function renderPage(title, date, file) {
 $.getJSON("content/index.json", function(post_list) {    
     var page_file = routePage();
     
-    if (!page_file)
-        page_file = Object.keys(post_list)[0]; // first item off the list
+    if (!page_file) page_file = Object.keys(post_list)[0]; // first item off the list
     
     $("#content").load("post.html", function() {
         renderPage(post_list[page_file].title, post_list[page_file].date, page_file);
